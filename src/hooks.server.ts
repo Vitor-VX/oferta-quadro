82ab5e5d6ae4518bfed02539f99fe9021eb3f0cd
Vitor-Vx <joaovictorrocha102019@gmail.com>
2026-03-06 16:53:51 -0400

    Removido proteção cloudlfare #3

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,18 @@
 export async function handle({ event, resolve }) {
     const origin = event.request.headers.get("origin")
     const pathname = event.url.pathname
+    const userAgent = event.request.headers.get("user-agent")
+    const referer = event.request.headers.get("referer")
+    const ip = event.getClientAddress()
 
-    console.log(origin);
-    console.log(pathname);
+    // Loga TUDO
+    console.log("=== REQUEST ===")
+    console.log("IP:", ip)
+    console.log("PATH:", pathname)
+    console.log("ORIGIN:", origin)
+    console.log("REFERER:", referer)
+    console.log("UA:", userAgent)
+    console.log("===============")
 
-    if (pathname.startsWith("/_app") || pathname.startsWith("/api")) {
-        if (origin && !origin.includes("imortalizesupet.shop")) {
-            return new Response("Forbidden", { status: 403 })
-        }
-        return resolve(event);
-    }
-
-    return resolve(event);
+    return resolve(event)
 }
